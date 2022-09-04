32dcd4632c7c7491d9333bbad877c46a490f8f1d
destinl <111584891+destinl@users.noreply.github.com>
2022-09-04 19:55:27 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// app.js
+//var common = require('../NewsDemo/utils/common.js')
+App({
+  globalData: {
+    ttile: '', //初始值
+  },
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    wx.setStorage({
+      key:"num",
+      data:1
+    })
+    
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+     
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  }
+})
+
